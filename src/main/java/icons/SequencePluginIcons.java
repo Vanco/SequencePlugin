3be6e0ec20ae835d2d17d70cab1b4353e6d725a1
cncsl <justitacsl@outlook.com>
2021-06-10 23:32:45 +0800

    feat: new icons 1. add new vector icons, and right icon for reGenerate bottom 2. delete old or unused icons 3. update version and CHANGELOG.md

--- src/main/java/icons/SequencePluginIcons.java
+++ src/main/java/icons/SequencePluginIcons.java
@@ -9,17 +9,17 @@ import javax.swing.*;
  * Created by van on 10/12/2016.
  */
 public interface SequencePluginIcons {
-    Icon SEQUENCE_ICON_13 = icon("/icons/sequence-13x13.png");
+    //svg can't display
     Icon SEQUENCE_ICON = icon("/icons/sequence-16x16.png");
-    Icon CLOSE_ICON = icon("/icons/close-16x16.png");
-    Icon EXPORT_ICON = icon("/icons/image-16x16.png");
-    Icon PREVIEW_ICON_13 = icon("/icons/preview-13x13.png");
-    Icon PREVIEW_ICON = icon("/icons/preview-16x16.png");
-    Icon REFRESH_ICON = icon("/icons/play-16x16.png");
-    Icon LOCKED_ICON = icon("/icons/locked.png");
-    Icon SAVE_ICON = icon("/icons/save-16x16.png");
-    Icon OPEN_ICON = icon("/icons/open-16x16.png");
-    Icon PUML_ICON = icon("/icons/puml-16x16.png");
+
+    Icon CLOSE_ICON = icon("/icons/close.svg");
+    Icon EXPORT_ICON = icon("/icons/image.svg");
+    Icon PREVIEW_ICON = icon("/icons/preview.svg");
+    Icon REFRESH_ICON = icon("/icons/refresh.svg");
+    Icon LOCKED_ICON = icon("/icons/lock.svg");
+    Icon SAVE_ICON = icon("/icons/save.svg");
+    Icon OPEN_ICON = icon("/icons/open.svg");
+    Icon PUML_ICON = icon("/icons/puml.svg");
 
     static Icon icon(String path) {return IconLoader.getIcon(path, SequencePluginIcons.class);}
 }
