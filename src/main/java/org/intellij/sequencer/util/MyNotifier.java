36952ad9db69d682eca2083e0f4c755e95971177
Vanco <van.fan@outlook.com>
2022-03-01 18:29:42 +0800

    Build for 221.x.

--- src/main/java/org/intellij/sequencer/util/MyNotifier.java
+++ src/main/java/org/intellij/sequencer/util/MyNotifier.java
@@ -1,18 +1,14 @@
 package org.intellij.sequencer.util;
 
-//import com.intellij.notification.NotificationDisplayType;
 import com.intellij.notification.NotificationGroup;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.AnAction;
-//import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.project.Project;
 import icons.SequencePluginIcons;
 import org.intellij.sequencer.SequenceService;
 import org.jetbrains.annotations.Nullable;
 
 public class MyNotifier {
-    private static final NotificationGroup NOTIFICATION_GROUP =
-            NotificationGroup.toolWindowGroup(SequenceService.PLUGIN_NAME, SequenceService.PLUGIN_NAME);
 
     /**
      * Notification user with content.
@@ -20,7 +16,7 @@ public class MyNotifier {
      * @param content the content max in three line, may have html tag as will
      */
     public static void notifyError(@Nullable Project project, String content) {
-        NOTIFICATION_GROUP
+        NotificationGroup.findRegisteredGroup(SequenceService.PLUGIN_NAME)
                 .createNotification(content, NotificationType.INFORMATION)
                 .setTitle(SequenceService.PLUGIN_NAME)
                 .setIcon(SequencePluginIcons.SEQUENCE_ICON_13)
@@ -34,7 +30,7 @@ public class MyNotifier {
      * @param action the action to be show in link.
      */
     public static void notifyWithAction(@Nullable Project project, String content, AnAction action) {
-        NOTIFICATION_GROUP
+        NotificationGroup.findRegisteredGroup(SequenceService.PLUGIN_NAME)
                 .createNotification(content, NotificationType.INFORMATION)
                 .setTitle(SequenceService.PLUGIN_NAME)
                 .setIcon(SequencePluginIcons.SEQUENCE_ICON_13)
